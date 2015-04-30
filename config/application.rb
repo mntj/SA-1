0576e186e56920050c8a3f8af9bc2ee1a5575f26
Corey Leveen <mntj11@gmail.com>
2015-04-29 22:09:44 -0400

    Add spec files

--- config/application.rb
+++ config/application.rb
@@ -16,6 +16,7 @@ Bundler.require(*Rails.groups)
 
 module SA1
   class Application < Rails::Application
+    config.autoload_paths << Rails.root.join('lib/classes')
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
---
+++ lib/classes/message_router.rb
@@ -0,0 +1,10 @@
+class MessageRouter
+  def initialize(raw_input)
+    @raw_input = raw_input
+  end
+
+  attr_reader :raw_input
+
+  def route_message
+  end
+end
