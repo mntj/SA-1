0da0aef937a7d06a316f7a4945e7a0260a4e2dce
Corey Leveen <mntj11@gmail.com>
2015-05-03 14:39:41 -0400

    Failing spec for Notetaker#retrieve_note

--- spec/lib/classes/notetaker_spec.rb
+++ spec/lib/classes/notetaker_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 RSpec.describe Notetaker do
   describe '#take_note' do
-    before :each do
+    before :all do
       Note.delete_all
     end
 
@@ -12,6 +12,16 @@ RSpec.describe Notetaker do
       notetaker = Notetaker.new(msg).take_note
 
       expect(Note.first.contents).to eq("read the sun also rises")
+    end
+  end
+
+  describe '#retrieve_note' do
+    it 'returns the correct note' do
+      msg = "View note 1"
+
+      note = Notetaker.new(msg).retrieve_note
+
+      expect(note).to eq("read the sun also rises")
     end
   end
 end
