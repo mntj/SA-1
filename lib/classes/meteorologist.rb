c9af149078d87cf34b320fdb79dc872819a7a563
Corey Leveen <mntj11@gmail.com>
2015-05-07 23:58:42 -0400

    Meteorologist#get_weather returns weather data

--- lib/classes/meteorologist.rb
+++ lib/classes/meteorologist.rb
@@ -6,7 +6,18 @@ class Meteorologist
   attr_reader :zip
 
   def get_weather
+    "General: #{weather_data[:main]}\n" <<
+    "Temp: #{weather_data[:temp]}"
+  end
+
+  private
+
+  def weather_data
     url = "http://api.openweathermap.org/data/2.5/weather?zip=#{zip},us"
     res = HTTParty.get(url)
+    {
+      :main => res["weather"].first["description"],
+      :temp => res["main"]["temp"]
+    }
   end
 end
--- spec/lib/classes/meteorologist_spec.rb
+++ spec/lib/classes/meteorologist_spec.rb
@@ -4,8 +4,8 @@ RSpec.describe Meteorologist do
   describe '#get_weather' do
     it 'retrieves weather data' do
       zip = 10010
-      data = Meteorologist.new(zip).get_weather
-      expect(data['weather']).not_to be_nil
+      weather = Meteorologist.new(zip).get_weather
+      expect(data).not_to be_nil
     end
   end
 end
